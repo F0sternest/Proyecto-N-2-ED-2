8e74305a743da83a79be44333f4643ea5355abb8
Kevin <kevin.rod2026@gmail.com>
2022-10-25 18:56:29 -0500

    Creacion de la red de ciudades y la impresion grafica

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import time
 from os import system
+import networkx as nx
+from pyvis.network import Network
 
 def obtenerOpcion():
     correcto = False
@@ -49,7 +51,42 @@ while exit:
 
         input()
     elif opcionMenuPrincipal == 2:
-        print('Crear red de ciudades')
+        grafoCiudades = nx.DiGraph()
+        nombresCiudades = []
+        caminoCiudades = []
+        valid = True
+
+        while valid:
+            try:
+                cantidadCiudades = int(input('Cuantas ciudades desea ingresar?: '))
+                valid = False
+            except ValueError:
+                valid = True
+                print('\nPorfavor ingrese un valor numerico.\n')
+        
+        for x in range(cantidadCiudades):
+            nombresCiudades.append(input(f'Ingrese el nombre de la ciudad {x+1}: '))
+        
+        print('Ahora porfavor escriba las rutas existentes entre las ciudades: ')
+        print('NOTA: Utilice el siguiente formato (Ciudad origen, Ciudad destino, Distancia en Km)')
+        caminoExit = True
+        while caminoExit:
+            camino = []
+            for i in range(2):
+                camino.append(input(f'Ingrese el nombre de la ciudad {i+1}: '))
+            camino.append(int(input('Cuantos kilometros tiene este camino?: ')))
+            camino = tuple(camino)
+            caminoCiudades.append(camino)
+
+            respExit = input('Desea ingresar otro camino? s/n: ')
+            if respExit.upper() == 'S':
+                caminoExit = True
+            else:
+                caminoExit = False
+        
+        grafoCiudades.add_nodes_from(nombresCiudades)
+        grafoCiudades.add_weighted_edges_from(caminoCiudades)
+        
         time.sleep(PAUSE_DURATION)
     elif opcionMenuPrincipal == 3:
         print('Ruta entre ciudades')
@@ -58,7 +95,9 @@ while exit:
         print('Ruta a partir de una ciudad a otra')
         time.sleep(PAUSE_DURATION)
     elif opcionMenuPrincipal == 5:
-        print('Imprimir graficamente')
+        canvasCiudades = Network('650px', '850px')
+        canvasCiudades.from_nx(grafoCiudades)
+        canvasCiudades.show('GraficoCiudades.html')
         time.sleep(PAUSE_DURATION)
     elif opcionMenuPrincipal == 6:
         exit = False
